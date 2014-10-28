4fd8ab765381307334e464931909a3943c3034fd
T.K. Baylis <tammy.baylis@gmail.com>
2014-10-28 13:20:32 -0700

    First working version of hellomouse.user.js

---
+++ hellomouse.user.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name        Hello Mouse
+// @namespace   http://cis.apsc.ubc.ca
+// @description popup image appears when mouseover Connect quick_links_wrap
+// @include 	  http*//connect.ubc.ca/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+// Include jQuery
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-latest.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+// div elements in Connect
+var bbf = document.getElementById('bbFrameset');
+var qlw = document.getElementById('quick_links_wrap');
+
+// css class for hover elements - does not seem to work in GM
+// var hs = document.createElement('style');
+// hs.type = 'text/css';
+// hs.innerHTML = '.hsClass{z-index: 9999; position: absolute; left: 0px; top: 0px;}';
+// bff.appendChild(hs);
+
+
+qlw.onmouseover = function() {
+  var hello = document.createElement('div');
+  var pug = document.createElement('img');
+  pug.setAttribute('src', 'http://www.i-love-pugs.com/images/PugPuppyFullView.jpg');
+  hello.appendChild(pug);
+  bbf.appendChild(hello);
+    
+  // This doesn't work
+//   hello.css({
+//     z-index: 9999;
+//     position: absolute;
+//     left: 0px;
+//     top: 0px;
+//   });
+
+  // This works, though not ideal
+  hello.style.zIndex = 9999;
+  hello.style.position = 'absolute';
+  hello.style.left = '250px';
+  hello.style.top = '250px';
+  hello.style.display = 'block';
+  
+}
+
+// qlw.onmouseout = function() {
+//  hello.style.display = 'none';
+//} 
